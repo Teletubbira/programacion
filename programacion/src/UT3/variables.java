425956571211d3fc146e5f3c8e03dc29d7bc82e1
Irasema <irapebe9@gmail.com>
2020-11-03 12:01:13 +0100

    Variables copiado de github de nayra

---
+++ programacion/src/UT3/variables.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package UT3;
+
+/**
+ * @author Irasema
+ *
+ */
+public class variables {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
