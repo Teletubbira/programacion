e4fc497d8ac48f73c051a012b47bb398f7040be9
Irasema <irapebe9@gmail.com>
2020-12-03 11:09:25 +0100

    Cosas de trimestral

--- programacion/src/UT3/pruebesita.java
+++ programacion/src/UT3/pruebesita.java
@@ -9,11 +9,30 @@ public class pruebesita {
 		
 		
 		
-		for (int i = 0; i < 100; i++) {
-			double r = Math.random() * 2;
-			int t = (int) r;
-			System.out.println(r + " " + t);
+		Scanner sc = new Scanner(System.in);
+		char[][] mapa = new char[10][10]; // X, H y puntos.
+
+		for (int i = 0; i < mapa.length; i++) {
+			for (int j = 0; j < mapa.length; j++) {
+				int n = (int) (Math.random() * 2); // generador random entre 0 y 1
+
+				switch (n) {
+
+				case 1:
+					mapa[i][j] = 'X';
+					break;
+
+				default:
+					mapa[i][j] = '.';
+					break;
+				}
+			}
+		}
+		for (int i = 0; i < mapa.length; i++) { 
+			for (int j = 0; j < mapa.length; j++) {
+				System.out.print(mapa[i][j] + " ");
+			}
+			System.out.print("\n");
 		}
 	}
-
 }
--- programacion/src/trimestral/cegato.java
+++ programacion/src/trimestral/cegato.java
@@ -6,9 +6,12 @@ public class cegato {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
 		char[][] mapa = new char[10][10]; // X, H y puntos.
+		char letra = 'P';
+		boolean enemigo = false;
+		int distancia = 0;
 
 		for (int i = 0; i < mapa.length; i++) {
 			for (int j = 0; j < mapa.length; j++) {
@@ -31,15 +34,71 @@ public class cegato {
 		System.out.println("Dame la posicion en columna del heroe: ");
 		int columna = sc.nextInt();
 		mapa[fila][columna] = 'H';
-		
-		//pintar mapa
-		for (int i = 0; i < mapa.length; i++) { 
+
+		// pintar mapa
+		for (int i = 0; i < mapa.length; i++) {
 			for (int j = 0; j < mapa.length; j++) {
 				System.out.print(mapa[i][j] + " ");
 			}
 			System.out.print("\n");
 		}
+
+		while (letra != 'X') { // condicion de salida
+			enemigo = false;
+			System.out.println("\nHacia que direccion mira? (R, L, U, D). X para salir.");
+			letra = sc.next().charAt(0);
+
+			switch (letra) {
+
+			case 'R':
+				for (int j = mapa[fila].length - 1; j > columna; j--) {
+					if (mapa[fila][j] == 'X') {
+						enemigo = true;
+						distancia = j - columna;
+					}
+				}
+				break;
+			case 'L':
+				for (int j = 0; j < columna; j++) {
+					if (mapa[fila][j] == 'X') {
+						enemigo = true;
+						distancia = columna - j;
+					}
+				}
+				break;
+			case 'U':
+				for (int i = 0; i < fila; i++) {
+					if (mapa[i][columna] == 'X') {
+						enemigo = true;
+						distancia = fila - i;
+					}
+				}
+				break;
+			case 'D':
+				for (int i = mapa.length - 1; i > fila; i--) {
+					if (mapa[i][columna] == 'X') {
+						enemigo = true;
+						distancia = i - fila;
+					}
+				}
+				break;
+			case 'X':
+				break;
+			default:
+				System.out.println("Letra erronea. Escribe otra: ");
+				break;
+
+			}
+			if (enemigo) {
+				System.out.println("El enemigo esta a una distancia de: " + distancia + ".\n");
+			} else {
+				if (letra != 'X')
+				System.out.println("No hay enemigo en esa direccion.");
+			}
+
+		}
+		System.out.println("Finalizando ejecucion.");
 		sc.close();
 	}
-	
+
 }
